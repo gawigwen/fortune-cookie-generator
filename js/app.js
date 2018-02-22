a71eb06fbb17dd992348ded2326789e6ef47c300
Dylan Tucker <dylantt2000@gmail.com>
2018-02-21 14:21:08 -1000

    3

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function click(){
+	var x = Math.floor((Math.random() * 10) + 1);
+    document.getElementById("fortune-cookie-text").innerHTML = x;
+}
+
+ document.getElementById("q").innerHTML = "2";
