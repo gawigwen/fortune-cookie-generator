c6cafe6010fd818c61d4f122a39ef99b1867df6c
gawigwen <19rodnce@waipahuh.k12.hi.us>
2018-02-22 18:53:42 -1000

    5

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
-function click(){
-	var x = Math.floor((Math.random() * 10) + 1);
-    document.getElementById("fortune-cookie-text").innerHTML = x;
-}
+var fortunes = [ 
+	"You will die",
+	"People will like you",
+	"You will find $100 under your bed",
+	"You will lose $200",
+	"People will hate you",
+	"Your bestfriend will backstab you",
+	"A clown will make your life a joke",
+	"You will get depression",
+	"You will lose all your ranked games",
+	"You will step on a lego",
+	"A lightning will struck you"
+];
 
- document.getElementById("q").innerHTML = "2";
+function clicked(){
+	var x = fortunes[Math.floor((Math.random() * 10) + 1)];//
+    document.getElementById("fortune-cookie-text").innerHTML = x;
+};
+
+
